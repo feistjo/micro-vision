560efa1ced5ee772f2dc9a7d54c05eca759578ef
Evan Allan <evanallan1026@gmail.com>
2021-11-19 14:58:34 -0600

    Add some logging

--- software/apps/micro-vision/main.c
+++ software/apps/micro-vision/main.c
@@ -26,46 +26,51 @@ static void i2c_reg_write(uint8_t i2c_addr, uint8_t reg_addr, uint8_t data) {
 }
 
 int main(void) {
-  printf("Board started!\n");
+  printf("\n==============\nBoard started!\n==============\nSleeping for 2 seconds to give the moto:bit time to initialize...");
   nrf_delay_ms(2000);
+  printf("done!\n");
 
   // Initialize I2C peripheral and driver
+  printf("Initializing I2C...");
   nrf_drv_twi_config_t i2c_config = NRF_DRV_TWI_DEFAULT_CONFIG;
   i2c_config.scl = EDGE_P19;
   i2c_config.sda = EDGE_P20;
   i2c_config.frequency = NRF_TWIM_FREQ_100K;
   i2c_config.interrupt_priority = 0;
   nrf_twi_mngr_init(&twi_mngr_instance, &i2c_config);
+  printf("done!\n");
 
-  printf("I2C initialized!\n");
-
+  printf("Initializing moto:bit...");
   moto_bit_init(&twi_mngr_instance);
+  printf("done!\n");
 
-  printf("moto:bit initialized\n");
+  printf("Moving forward\n");
+  moto_bit_set_speed(1.0f);
+  nrf_delay_ms(2000);
+
+  printf("Turning left\n");
+  moto_bit_turn(90, 0.25f);
 
   printf("Moving forward\n");
   moto_bit_set_speed(0.5f);
   nrf_delay_ms(2000);
 
-  printf("Turning left\n");
-  moto_bit_turn(90, 0.5f);
-
-  printf("Forward\n");
-  moto_bit_set_speed(0.5f);
-  nrf_delay_ms(2000);
-
+  printf("Stopping\n");
   moto_bit_stop();
-  nrf_delay_ms(2000);
+  nrf_delay_ms(1000);
 
+  printf("Moving backward\n");
   moto_bit_set_speed(-0.5f);
   nrf_delay_ms(2000);
 
-  moto_bit_turn(-90, 0.5f);
-  //nrf_delay_ms(1000);
+  printf("Turning right\n");
+  moto_bit_turn(-90, 0.25f);
 
-  moto_bit_set_speed(-0.5f);
+  printf("Moving backward\n");
+  moto_bit_set_speed(-1.0f);
   nrf_delay_ms(2000);
 
+  printf("Stopping\n");
   moto_bit_stop();
 
   while (1) {
